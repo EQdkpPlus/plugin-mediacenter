4dabca150971d478e2a94415cc6611a86de45608
GodMod <godmod23@web.de>
2019-06-20 12:11:45 +0200

    Group notifications

--- mediacenter_plugin_class.php
+++ mediacenter_plugin_class.php
@@ -42,7 +42,7 @@ class mediacenter extends plugin_generic
   }
 
 //This Version is for EQdkp Plus 2.3.x
-  public $version    = '2.2.8';
+  public $version    = '2.2.9';
   public $build      = '';
   public $copyright  = 'GodMod';
   public $vstatus    = 'Alpha';
